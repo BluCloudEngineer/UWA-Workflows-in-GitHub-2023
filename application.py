8cfe6aabcc6271b22d651bcdf77700016548fe10
BluCloudEngineer <thatawscloudengineer@gmail.com>
2023-08-19 20:40:38 +0800

    Added skeleton code

--- application.py
+++ application.py
@@ -1,16 +1,26 @@
 # Imports
-from flask import Flask
+from flask import Flask, render_template, request
 
 # Initialise the Flask application
 application = Flask(__name__) # You must use the word "application" for this to work in AWS
 
 # Add routes
 @application.route("/")
-def default_route():
-    """
-    Return a string of "Hello world!"
-    """
-    return "Hello world!"
+def index():
+    return render_template("index.html")
+
+@application.route("/calculate", methods=["POST"])
+def calculate():
+    # Required values
+    result = None
+    operation = request.form["operation"]
+    #number_1 = float(request.form["number_1"])
+    #number_2 = float(request.form["number_2"])
+
+    # Perform logic
+    # TODO
+
+    return render_template("index.html", result = result)
 
 
 
