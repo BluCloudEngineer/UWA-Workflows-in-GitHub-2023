507ad318aef2e96a5d08733416755abd895ae886
Hieu Nguyen <hieu.h.t.nguyen@tcu.edu>
2026-03-19 17:42:13 +0800

    Convert F-C (#127)
    * done convert F-C
    
    * done convert C-F

--- application.py
+++ application.py
@@ -46,6 +46,11 @@ def calculate():
     if operation == "addition":
         number_2 = float(request.form["number_2"])
         result = number_1 + number_2
+       # Perform convert to fahrenheit operations       
+    if operation == "celsius_to_fahrenheit":
+        result = str((number_1 * 9/5) + 32) + "°F"
+
+
     if operation == "natural_log":
         result = math.log(number_1)
     return render_template("index.html", result=result)
